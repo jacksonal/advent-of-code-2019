2e15af9aa3a5a7ac95cd70212ba91b024b50c9f0
Alex Jackson <alex.lee.jackson@gmail.com>
2019-12-08 13:16:36 -0500

    day 5 part 1

--- src/advent_of_code_2019/day2.clj
+++ src/advent_of_code_2019/day2.clj
@@ -1,18 +1,80 @@
-(defn operate
+
+(defn get-op-code
+  [instruction]
+  (mod instruction 100))
+
+(defn get-param-mode
+  [instruction pos]
+  (mod (quot instruction (int (Math/pow 10 (+ 1 pos)))) 10))
+
+(defn get-param-value
+  [full-input op-seq pos]
+  (let [param (nth op-seq pos)]
+    (case (get-param-mode (nth op-seq 0) pos)
+      1 param
+      0 (nth full-input param))))
+(defn add 
   [full-input op-seq]
   (into
    (into (subvec full-input 0 (nth op-seq 3))
-         (if (== 1 (nth op-seq 0))
-           (vector (+ (nth full-input (nth op-seq 1)) (nth full-input (nth op-seq 2))))
-           (if (== 2 (nth op-seq 0))
-             (vector (* (nth full-input (nth op-seq 1)) (nth full-input (nth op-seq 2)))))))
+         (vector (+ (get-param-value full-input op-seq 1) (get-param-value full-input op-seq 2))))
    (subvec full-input (+ (nth op-seq 3) 1))))
+(defn mult
+  [full-input op-seq]
+  (into
+   (into (subvec full-input 0 (nth op-seq 3))
+         (vector (* (get-param-value full-input op-seq 1) (get-param-value full-input op-seq 2))))
+   (subvec full-input (+ (nth op-seq 3) 1))))
+
+(defn write
+  [full-input op-seq]
+  (println "INPUT REQUIRED")
+  (into
+   (into (subvec full-input 0 (nth op-seq 1))
+         (vector (Integer/parseInt (read-line))))
+   (subvec full-input (+ (nth op-seq 1) 1))))
+
+(defn output
+  [full-input op-seq]
+  ;(println "output op-seq:" op-seq)
+  (println "OUTPUT:" (get-param-value full-input op-seq 1))
+  full-input)
+
+(defn operate
+  [full-input op-seq]
+  (let [op-code (mod (get-op-code (nth op-seq 0)) 10)] 
+    (case op-code
+      1 (add full-input op-seq)
+      2 (mult full-input op-seq)
+      3 (write full-input op-seq)
+      4 (output full-input op-seq))))
+
+(defn get-op-size
+  [op-code]
+  (case op-code
+    (1 2) 4
+    (3 4) 2))
 
 (defn recur-int-compute
   [full-input start]
-  (if (== 99 (nth full-input start))
-    full-input
-    (recur (operate full-input (subvec full-input start (+ start 4))) (+ start 4))))
+  ;(println "program" full-input)
+  (let [instruction (get-op-code (nth full-input start))]
+    (if (= 99 instruction)
+      full-input
+      (let [op-size (get-op-size (mod instruction 10))]
+        (recur (operate full-input 
+                        (subvec full-input start (+ start op-size)))
+               (+ start op-size))))))
+
+(defn run-diagnostic
+  "day 5 part 1"
+  []
+  (let [input (into (vector) (map #(Integer/parseInt %) (clojure.string/split
+                                                         (slurp "resources/input_day5.txt")
+                                                         #",")))]
+;(operate input [3 225])))
+    (recur-int-compute input 0)
+    (println "Done")))
 
 (defn int-compute
   []
