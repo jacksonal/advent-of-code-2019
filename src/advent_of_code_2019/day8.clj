a6dc83bd533484738db29bf83fdf3a14cd00b825
Alex Jackson <ajackson@ebsco.com>
2019-12-16 16:24:05 -0500

    define apply mask function

--- src/advent_of_code_2019/day8.clj
+++ src/advent_of_code_2019/day8.clj
@@ -17,7 +17,20 @@
               (inc seed)
               seed)) 0 layer)])
 
+(defn apply-mask
+  [top bottom]
+  (let [flat-top (flatten top)
+        flat-bottom (flatten bottom)]
+    (map (fn [[top-pixel bottom-pixel]]
+           (if (= top-pixel 2)
+             bottom-pixel
+             top-pixel)) (map vector flat-top flat-bottom))))
+  
+(defn combine-layers
+  [layers]
+  (reduce apply-mask layers))
 (defn check-image-integrity
+  "find the layer with the fewest 0s and multiply the number of 1s by the number of 2s"
   []
   (let [input (into (vector) (map #(Integer/parseInt %) (clojure.string/split
                                                          (slurp "resources/input_day8.txt")
@@ -25,3 +38,12 @@
         layers (partition 6 (partition 25 input))
         min-zero-layer (apply min-key #(first %) (map count-digits (map flatten layers)))]
     (* (nth min-zero-layer 1) (nth min-zero-layer 2))))
+
+(defn view-image
+  "0 - black 1 - white 2 - transparent"
+  []
+  (let [input (into (vector) (map #(Integer/parseInt %) (clojure.string/split
+                                                         (slurp "resources/input_day8.txt")
+                                                         #"")))
+        layers (partition 6 (partition 25 input))]
+    (combine-layers layers)))
