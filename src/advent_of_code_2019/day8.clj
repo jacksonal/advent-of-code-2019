7a7bc172d0b92948c5670dc28679f7364ec8a706
Alex Jackson <ajackson@ebsco.com>
2019-12-16 14:37:10 -0500

    solving day8 part 1

--- src/advent_of_code_2019/day8.clj
+++ src/advent_of_code_2019/day8.clj
@@ -1,15 +1,27 @@
-(defn count-zeros
+(defn count-digits
+  "reduce to a tuple of number of 0s, 1s, and 2s"
   [layer]
-  (reduce (fn 
+  [(reduce (fn 
             [seed pixel]
             (if (zero? pixel)
               (inc seed)
-              seed)) 0 layer))
+              seed)) 0 layer)
+  (reduce (fn
+            [seed pixel]
+            (if (= pixel 1)
+              (inc seed)
+              seed)) 0 layer)
+  (reduce (fn
+            [seed pixel]
+            (if (= pixel 2)
+              (inc seed)
+              seed)) 0 layer)])
 
 (defn check-image-integrity
   []
   (let [input (into (vector) (map #(Integer/parseInt %) (clojure.string/split
                                                          (slurp "resources/input_day8.txt")
                                                          #"")))
-        layers (partition 6 (partition 25 input))]
-    (apply min (map count-zeros (map flatten layers)))))
+        layers (partition 6 (partition 25 input))
+        min-zero-layer (apply min-key #(first %) (map count-digits (map flatten layers)))]
+    (* (nth min-zero-layer 1) (nth min-zero-layer 2))))
